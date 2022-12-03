65c6d4ba7defdd630a6d02df0c29ccaaf5ff9abc
Vilmar Ferreira <vilmar.jr86@gmail.com>
2022-12-03 19:54:47 -0300

    utilizando hooks em functionals components

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TestComponent.css';
 
-const TestComponent = () => {
-    return <div className='TestComponent'>Test Component</div>
+
+
+const TestComponent = (props: { name: string, colaborador: string }) => {
+
+    const [age, setAge] = useState(36)
+    
+    return (<div className='TestComponent'>Olá, {props.name}. <br/>
+            O seu colaborador { props.colaborador } possui { age } anos <br/>
+            <button onClick={() => {
+                {
+                    setAge(age + 1)
+                }
+            }}>
+                Incrementar
+            </button>
+
+        </div>
+    )
 }
 
 export default TestComponent
