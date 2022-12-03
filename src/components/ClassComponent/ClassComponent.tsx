a271aadfcdac877fcc96c38294a15c96551bb035
Vilmar Ferreira <vilmar.jr86@gmail.com>
2022-12-03 19:28:14 -0300

    gerenciando ciclo de vida react - mounting, updating e unmounting

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -2,11 +2,27 @@ import React from 'react'
 
 class ClassComponent extends React.Component<{ name: string }> {
 
+    constructor(props: any) {
+        super(props)
+        console.log('constructor reached')
+    }
+
     state = {
         name: 'Vilmar e Joana'
     }
+
+    componentDidMount(): void {
+        console.log('DidMount reached')
+    }
+
+    componentDidUpdate(): void {
+        console.log('DidUpdate reached')
+    }
     
     render() {
+
+        console.log('render reached')
+
         return <div>
             <p>name: { this.state.name }</p>
             <button onClick={() => {
