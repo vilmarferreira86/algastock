1005bb427463f6a76b20d2f051d1b846bcc1b327
Vilmar Ferreira <vilmar.jr86@gmail.com>
2022-12-03 20:04:13 -0300

    disable class compnent from app and add useEffect hook

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './TestComponent.css';
 
 
@@ -7,6 +7,14 @@ const TestComponent = (props: { name: string, colaborador: string }) => {
 
     const [age, setAge] = useState(36)
     
+    useEffect(() => {
+        console.log('Component was created')
+    }, [])
+
+    useEffect(() => {
+        console.log('Age has been updated to: ' + age)
+    }, [age])
+
     return (<div className='TestComponent'>Olá, {props.name}. <br/>
             O seu colaborador { props.colaborador } possui { age } anos <br/>
             <button onClick={() => {
