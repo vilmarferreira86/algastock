2fe13cda6156644d2dbf3e8fe5256aca307329e2
Vilmar Ferreira <vilmar.jr86@gmail.com>
2022-12-03 21:08:52 -0300

    add header component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from '../../logo.svg';
+import Header from '../Header';
 import './App.css';
-import TestComponent from '../TestComponent';
-// import ClassComponent from '../ClassComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TestComponent name="BNB" colaborador='Vilmar'/>
-        {/* <ClassComponent name="Mundo"/> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title='AlgaStock'/>
     </div>
   );
 }
